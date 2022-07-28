f90a14aa1051a113229d65d5eb0ebad4d9ec9522
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-07-28 12:25:44 +0530

    Create 242. Valid Anagram.java

---
+++ 242. Valid Anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length()) 
+            return false;
+        
+        int n = s.length();
+        
+        char s1[] = s.toCharArray();   // Convert the string to char array
+        char t1[] = t.toCharArray();
+        
+        Arrays.sort(s1);   // Sort the arrays
+        Arrays.sort(t1);
+        
+        for ( int i = 0; i < n; i++ ){    // If both sorted arrays are equal --> return true
+            if( s1[i] != t1[i] ) return false;
+        }
+        return true;
+    }
+}
