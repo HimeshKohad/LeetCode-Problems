2ac404968da8cd79b00952480726d91a60632a12
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-07-31 15:50:46 +0530

    Create 189. Rotate Array .cpp

---
+++ 189. Rotate Array .cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector <int> temp(n);
+        
+        for (int i = 0; i < n; i++){
+            temp[(i + k) % n] = nums[i];
+        }
+        
+        nums = temp;
+    }
+};
