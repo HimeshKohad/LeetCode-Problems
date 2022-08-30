2db3e10ff70f31b0b64ff8a71fc2d7a3dba478e1
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-08-30 12:42:06 +0530

    Update and rename 231. Power of Two.java to 231. Power of Two/Java Solution.java

--- 231. Power of Two/Java Solution.java
+++ 231. Power of Two/Java Solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n<=0){
+        if (n <= 0){
             return false;
         }
         if (n == 1){
             return true;
         }
-        if (n%2 != 0){
+        if (n % 2 != 0){
             return false;
         }
         return isPowerOfTwo(n/2);
