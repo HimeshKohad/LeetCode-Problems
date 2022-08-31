b797ad0e3d047873c601622ac82a81950607a29a
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-08-31 10:44:51 +0530

    Create Cpp Solution.cpp

---
+++ 154. Find Minimum in Rotated Sorted Array II/Cpp Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        // Binary search
+        int s = 0; 
+        int e = nums.size() - 1;
+        
+        while (s <= e){
+            int mid = s + (e - s)/2;
+            if(nums[mid] > nums[e]){
+                s = mid + 1;
+            }
+            else
+                e--;
+        }
+        return nums[s];
+    }
+};
