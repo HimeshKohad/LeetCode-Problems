1c40e3b471830e565415523020c5c26ea65adb2c
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-08-31 10:26:35 +0530

    Create Cpp Solution.cpp

---
+++ 54. Spiral Matrix/Cpp Solution.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int> >& matrix) {
+        
+        
+        vector<int> ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int count = 0;
+        int total = row*col;
+        
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row-1;
+        int endingCol = col-1;
+        
+        
+        while(count < total) {
+            
+            // Print Starting Row
+            for(int index = startingCol; count < total && index<=endingCol; index++) {
+                ans.push_back(matrix[startingRow][index]);
+                count++;
+            }
+            startingRow++;
+            
+            // Print Ending Column
+            for(int index = startingRow; count < total && index<=endingRow; index++) {
+                ans.push_back(matrix[index][endingCol]);
+                count++;
+            }
+            endingCol--;
+            
+            // Print Ending Row
+            for(int index = endingCol; count < total && index>=startingCol; index--) {
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            }
+            endingRow--;
+            
+            // Print Starting Column
+            for(int index = endingRow; count < total && index>=startingRow; index--) {
+                ans.push_back(matrix[index][startingCol]);
+                count++;
+            }
+            startingCol++;
+        } 
+        return ans;
+    }
+};
