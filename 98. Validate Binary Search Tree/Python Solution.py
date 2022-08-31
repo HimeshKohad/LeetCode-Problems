db72d6f88570337aebb8cc0a205dc1313d6f7dc2
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-08-31 10:41:21 +0530

    Create Python Solution.py

---
+++ 98. Validate Binary Search Tree/Python Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        def valid ( node, left, right ):
+            if not node:
+                return True
+            
+            if not ( node.val < right and node.val > left ):
+                return False
+            
+            return ( valid ( node.left, left, node.val ) and
+                   valid ( node.right, node.val, right ) )
+        
+        return valid ( root, float("-inf"), float("inf") )
