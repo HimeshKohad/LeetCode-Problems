a8dd1987e6f5f589a1bbd60aa87060aa518fe602
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-09-01 09:52:35 +0530

    Create Cpp Solution.cpp

---
+++ 275. H-Index II/Cpp Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        
+        int n = citations.size();
+        int s = 0, e = n;
+        
+        while (s < e){
+            int mid = s + (e - s)/2;
+            
+            if (n - mid <= citations[mid])
+                e = mid;
+            
+            else 
+                s = mid + 1;
+            
+        }
+        
+        return n - e;
+        
+    }
+};
