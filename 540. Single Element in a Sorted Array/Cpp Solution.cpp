32c99619e314d399c933c2ab4e922d68aee0b096
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-09-02 08:58:06 +0530

    Create Cpp Solution.cpp

---
+++ 540. Single Element in a Sorted Array/Cpp Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        
+        int s = 0, e = nums.size() - 1;
+        
+        while (s < e){
+            int mid = s + (e - s)/2;
+            if (mid % 2 == 1) mid--;
+            
+            if (nums[mid] != nums[mid + 1]){
+                e = mid;
+            } 
+            
+            else s = mid + 2;
+        }
+        
+        return nums[s];
+    }
+};
