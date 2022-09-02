4b1e293a97990e1e0d1650f53204db8ce236ea60
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-09-02 09:35:31 +0530

    Create Cpp Solution.cpp

---
+++ 2169. Count Operations to Obtain Zero/Cpp Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        
+        long count = 0;
+        
+        while (num1 != 0 && num2 != 0 ){
+            if(num1 > num2){
+                num1 -= num2;
+            }
+            else 
+                num2 -= num1;
+            
+            count++;
+        }
+        
+        return count;  
+    }
+};
