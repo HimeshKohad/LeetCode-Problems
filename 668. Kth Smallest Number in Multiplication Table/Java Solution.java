09caee577668b5866db9362e0166eac5eb2d2dad
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-09-02 09:04:00 +0530

    Create Java Solution.java

---
+++ 668. Kth Smallest Number in Multiplication Table/Java Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findKthNumber(int m, int n, int k) {
+        
+        int s = 1, e = m * n;
+        int ans = 0;
+        
+        while (s <= e){
+            int mid = s + (e - s)/2;
+            int count = 0;
+            
+            for (int i = 1; i <= n; i++){
+                count += Math.min(mid/i, m);
+            }
+            
+            if (count >= k){
+                ans = mid;
+                e = mid - 1;
+            }
+            
+            else {
+                s = mid + 1;
+            }
+        }
+        
+        return ans;
+        
+    }
+}
