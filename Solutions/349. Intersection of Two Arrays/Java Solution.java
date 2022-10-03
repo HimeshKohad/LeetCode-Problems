63e526709313aee0e5f4a86a44297411b30c0cdf
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-10-03 08:57:38 +0530

    Create Java Solution.java

---
+++ Solutions/349. Intersection of Two Arrays/Java Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        
+        HashSet<Integer> set1 = new HashSet<Integer>();
+        for (Integer n : nums1) set1.add(n);
+        
+        HashSet<Integer> set2 = new HashSet<Integer>();
+        for (Integer n : nums2) set2.add(n);
+
+        set1.retainAll(set2);
+
+        int [] ans = new int[set1.size()];
+        
+        int idx = 0;
+        
+        for (int s : set1) ans[idx++] = s;
+        
+        return ans;
+        
+    }
+}
