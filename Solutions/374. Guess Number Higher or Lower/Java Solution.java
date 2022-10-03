d6d12f694a94ab98b198f196927adaa0d06dfb35
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2022-10-03 09:02:32 +0530

    Create Java Solution.java

---
+++ Solutions/374. Guess Number Higher or Lower/Java Solution.java
@@ -0,0 +1,25 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int s = 1, e = n;
+        int mid = s + (e - s)/2;
+        
+        while (s <= e){
+            
+            if (guess(mid) == 0){
+                return mid;
+            }
+            
+            else if (guess(mid) == -1){
+                e = mid - 1;
+            }
+            
+            else if (guess(mid) == 1){
+                s = mid +1;
+            }
+            
+            mid = s + (e - s)/2;
+        }
+        
+        return mid;
+    }
+}
