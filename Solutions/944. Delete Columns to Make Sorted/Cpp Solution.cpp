8103fe0dcfb60c974db3405316e0502711bd0877
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2023-01-04 20:17:04 +0530

    Create Cpp Solution.cpp

---
+++ Solutions/944. Delete Columns to Make Sorted/Cpp Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        
+        int cnt = 0;
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 0; j < strs.size() - 1; j++) {
+                if (strs[j][i] > strs[j + 1][i]) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
