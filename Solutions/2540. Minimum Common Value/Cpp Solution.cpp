0f974050ae873ca8875b420320414cda88319cae
AkhileshPokale-IND <81848820+AkhileshPokale-IND@users.noreply.github.com>
2023-01-23 21:06:55 +0530

    2540. Minimum Common Value

---
+++ Solutions/2540. Minimum Common Value/Cpp Solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        
+        int i = 0, j = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+
+            if (nums1[i] < nums2[j]) {
+                i++;
+            }
+
+            else if (nums1[i] > nums2[j]) {
+                j++;
+            }
+
+            else {
+                return nums1[i];
+            }
+        }
+
+        return -1;
+        
+    }
+};
