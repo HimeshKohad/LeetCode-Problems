dbb63ede96832f93b90e3476667de5fa3ee8f6d2
Himesh Kohad <107066424+HimeshKohad@users.noreply.github.com>
2023-07-08 20:32:57 +0530

    Create Cpp Solution.cpp

---
+++ Solutions/543. Diameter of Binary Tree/Cpp Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int height(TreeNode* node, int& diameter) {
+        if (!node) return 0;
+
+        int lh = height(node -> left, diameter);
+        int rh = height(node -> right, diameter);
+
+        diameter = max(diameter, lh + rh);
+        return 1 + max(lh, rh);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root, diameter);
+
+        return diameter;
+    }
+};
